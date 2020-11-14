941e8800f9d631755a03f17641a5de4911d30804
VoidInSpace <amlagniton@iskolarngbayan.pup.edu.ph>
2020-11-14 19:05:33 +0800

    added basic functions

---
+++ main.lua
@@ -0,0 +1,71 @@
+push = require 'push'
+class = require 'class'
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+
+VIRTUAL_WIDTH = 432
+VIRTUAL_HEIGHT = 243
+
+function love.load()
+    love.graphics.setDefaultFilter('nearest', 'nearest')
+
+    love.window.setTitle('Pong')
+
+    math.randomseed(os.time())
+
+    smallFont = love.graphics.newFont('Assets/font.ttf', 8)
+    largeFont = love.graphics.newFont('Assets/font.ttf', 16)
+    scoreFont = love.graphics.newFont('Assets/font.ttf', 32)
+    love.graphics.setFont(smallFont)
+
+    sounds = {
+        ['paddle_hit'] = love.audio.newSource('Assets/Hit_Hurt2.wav', 'static'),
+        ['score'] = love.audio.newSource('Assets/Pickup_Coin.wav', 'static'),
+        ['wall_hit'] = love.audio.newSource('Assets/Explosion.wav', 'static')
+    }
+
+    push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
+        fullscreen = false,
+        resizable = true,
+        vsync = true
+    })
+
+    player1Score = 0
+    player2Score = 0
+end
+
+function love.resize(w, h)
+    push:resize(w, h)
+end
+
+function love.update(dt)
+
+end
+
+function love.keypressed(key)
+    if key == 'escape' then
+        love.event.quit()
+    end
+end
+
+function love.draw()
+    push:apply('start')
+
+    love.graphics.clear(40/255, 45/255, 52/255, 1)
+
+    love.graphics.setFont(scoreFont)
+    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 -50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(player2Score), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
+    love.graphics.setFont(smallFont)
+
+    displayFPS()
+
+    push:apply('end')
+end
+
+function displayFPS()
+    love.graphics.setFont(smallFont)
+    love.graphics.setColor(0, 255, 0, 255)
+    love.graphics.print('FPS: ' .. tostring(love.timer.getFPS()), 10, 10)
+end

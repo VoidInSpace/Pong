58af968446119aa47d785366ace81ea29e035d89
VoidInSpace <amlagniton@iskolarngbayan.pup.edu.ph>
2020-11-20 23:39:23 +0800

    edited winning and serving player messages

--- main.lua
+++ main.lua
@@ -1,12 +1,17 @@
+--the word 'require' is used to get the properties of the other files and use
+--it in the main file
 push = require 'push'
 Class = require 'class'
 
 require 'Paddle'
 require 'Ball'
 
+--width and height of the screen 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
 
+--virtual width and virtual height has the same resoulution, either in a small 
+--or large screen
 VIRTUAL_WIDTH = 432
 VIRTUAL_HEIGHT = 243
 
@@ -36,15 +41,17 @@ function love.load()
         vsync = true
     })
 
-    player1Score = 0
+    playerScore = 0
     botScore = 0
 
     servingPlayer = 1
     winningPlayer = 0
 
-    player1 = Paddle(10, 30, 5, 20)
+    --position of the paddles virtually
+    player = Paddle(10, 30, 5, 20)
     bot = Paddle(VIRTUAL_WIDTH - 10, VIRTUAL_HEIGHT -30, 5, 20)
 
+    --position of the ball virtually
     ball = Ball(VIRTUAL_WIDTH / 2 - 2, VIRTUAL_HEIGHT / 2 - 2, 4, 4)
 
     gameState = 'start'
@@ -63,9 +70,9 @@ function love.update(dt)
             ball.dx = -math.random(140, 200)
         end
     elseif gameState == 'play' then
-        if ball:collides(player1) then
+        if ball:collides(player) then
             ball.dx = -ball.dx * 1.05
-            ball.x = player1.x + 5
+            ball.x = player.x + 5
 
             if ball.dy < 0 then
                 ball.dy = -math.random(10, 150)
@@ -117,10 +124,10 @@ function love.update(dt)
 
         if ball.x > VIRTUAL_WIDTH then
             servingPlayer = 2
-            player1Score = player1Score + 1
+            playerScore = playerScore + 1
             sounds['score']:play()
 
-            if player1Score == 10 then
+            if playerScore == 10 then
                 winningPlayer = 1
                 gameState = 'done'
             else
@@ -130,20 +137,22 @@ function love.update(dt)
         end
     end
     
+    --for the paddle controlled by the player (left paddle)
     if love.keyboard.isDown('w') then
-        player1.dy = -PADDLE_SPEED
+        player.dy = -PADDLE_SPEED
     elseif love.keyboard.isDown('s') then
-        player1.dy = PADDLE_SPEED
+        player.dy = PADDLE_SPEED
     else
-        player1.dy = 0
+        player.dy = 0
     end
 
+    --for the paddle contolled by AI (right paddle)
     if gameState == 'play' then
         if ball.y > bot.y and ball.y + ball.height < bot.y  + bot.height then 
             bot.dy = 0
         elseif bot.y > ball.y + ball.height then
             bot.dy = -PADDLE_SPEED
-        elseif bot.y +bot.height < ball.y then
+        elseif bot.y + bot.height < ball.y then
         bot.dy = PADDLE_SPEED
         end
     else
@@ -154,7 +163,7 @@ function love.update(dt)
         ball:update(dt)
     end
 
-    player1:update(dt)
+    player:update(dt)
     bot:update(dt)
 end
 
@@ -177,7 +186,7 @@ function love.keypressed(key)
             ball:reset()
 
             
-            player1Score = 0
+            playerScore = 0
             botScore = 0
 
             
@@ -193,6 +202,7 @@ end
 function love.draw()
     push:apply('start')
 
+    --for the background color 
     love.graphics.clear(79/255, 98/255, 122/255, 1)
 
     if gameState == 'start' then
@@ -201,21 +211,26 @@ function love.draw()
         love.graphics.printf('Press Enter to begin!', 0, 20, VIRTUAL_WIDTH, 'center')
     elseif gameState == 'serve' then
         love.graphics.setFont(smallFont)
-        love.graphics.printf('Player ' .. tostring(servingPlayer) .. "'s serve!", 
-            0, 10, VIRTUAL_WIDTH, 'center')
+        if servingPlayer == 1 then
+            love.graphics.printf("Player's serve", 0, 10, VIRTUAL_WIDTH, 'center')
+        else 
+            love.graphics.printf("Bot's serve", 0, 10, VIRTUAL_WIDTH, 'center')
+        end
         love.graphics.printf('Press Enter to serve!', 0, 20, VIRTUAL_WIDTH, 'center')
     elseif gameState == 'play' then
     elseif gameState == 'done' then
         love.graphics.setFont(largeFont)
-        love.graphics.printf('Player ' .. tostring(winningPlayer) .. ' wins!',
-            0, 10, VIRTUAL_WIDTH, 'center')
-        love.graphics.setFont(smallFont)
+        if winningPlayer == 1 then
+            love.graphics.printf("Player wins", 0, 10, VIRTUAL_WIDTH, 'center')
+        else 
+            love.graphics.printf("Bot wins", 0, 10, VIRTUAL_WIDTH, 'center')
+        end
         love.graphics.printf('Press Enter to restart!', 0, 30, VIRTUAL_WIDTH, 'center')
     end
 
     displayScore()
 
-    player1:render()
+    player:render()
     bot:render()
 
     ball:render()
@@ -227,10 +242,8 @@ end
 
 function displayScore()
     love.graphics.setFont(scoreFont)
-    love.graphics.print(tostring(player1Score), VIRTUAL_WIDTH / 2 - 50,
-        VIRTUAL_HEIGHT / 3)
-    love.graphics.print(tostring(botScore), VIRTUAL_WIDTH / 2 + 30,
-        VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(playerScore), VIRTUAL_WIDTH / 2 - 50, VIRTUAL_HEIGHT / 3)
+    love.graphics.print(tostring(botScore), VIRTUAL_WIDTH / 2 + 30, VIRTUAL_HEIGHT / 3)
 end
 
 function displayFPS()
